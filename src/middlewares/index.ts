16d0334848e9e473dd6775b34212ffd82cb66cd6
Matheus Azambuja <matheushenrique.ads@gmail.com>
2021-03-31 14:47:08 -0300

    feat(db): add middleware to connect on database

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,4 +1,6 @@
 import httpJsonBodyParser from '@middy/http-json-body-parser'
 import JSONErrorHandlerMiddleware from 'middy-middleware-json-error-handler'
+import HttpErrorHandler from '@middy/http-error-handler'
+import { DatabaseConnection } from './DatabaseConnection'
 
-export default [httpJsonBodyParser(), JSONErrorHandlerMiddleware()]
+export default [HttpErrorHandler(), httpJsonBodyParser(), JSONErrorHandlerMiddleware(), DatabaseConnection()]
