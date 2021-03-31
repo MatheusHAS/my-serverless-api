9fe5a4793be8db4ac92da5474a8f82abeff33105
Matheus Azambuja <matheushenrique.ads@gmail.com>
2021-03-31 14:47:53 -0300

    feat(user): add simple registration (need refact)

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -2,14 +2,34 @@ import { APIGatewayEvent, Callback } from 'aws-lambda'
 import middy from '@middy/core'
 import validator from '@middy/validator'
 import middlewares from '@/middlewares'
-import { UserValidatorSchema } from '@/models'
+import { UserValidatorSchema, UserDocument } from '@/models'
+import UserModel from '@/models/User'
 
-export const register = middy((event: APIGatewayEvent, context: any, callback: Callback) =>
-  callback(null, {
-    body: JSON.stringify({
-      example: true,
-    }),
+export const register = middy(async (event: APIGatewayEvent, context: any, callback: Callback) => {
+  const { email, password } = event.body as any
+  const existUser: any = await UserModel.findOne({
+    email,
   })
-)
+
+  if (existUser && existUser._id) {
+    return callback(null, {
+      body: JSON.stringify({
+        error: true,
+        message: 'User already exist',
+        statusCode: 400,
+      }),
+    })
+  }
+  const newUser = new UserModel({
+    email,
+    password,
+  })
+  newUser.save((error: any, doc: any) => {
+    console.log(error)
+  })
+  return callback(null, {
+    body: JSON.stringify(newUser),
+  })
+})
   .use(middlewares)
   .use(validator({ inputSchema: UserValidatorSchema }))
